f0593b892be4dbe02c27eed9e2d0e738b4f74b43
Elliot Braem <elliot@ejlbraem.com>
2024-10-23 22:42:52 -0400

    memo sheet stack

--- src/components/common/sheet-stack.tsx
+++ src/components/common/sheet-stack.tsx
@@ -19,7 +19,57 @@ import {
 } from "@/components/ui/sheet";
 import { useMediaQuery } from "@/hooks/use-media-query";
 import { useSheetStack } from "@/hooks/use-sheet-stack";
-import { ReactNode } from "react";
+import { ReactNode, memo } from "react";
+
+const StackItem = memo(function StackItem({
+  isMobile,
+  children,
+  closeSheet
+}: {
+  isMobile: boolean;
+  children: ReactNode;
+  closeSheet: () => void;
+}) {
+  if (isMobile) {
+    return (
+      <Drawer open={true} onOpenChange={closeSheet}>
+        <DrawerContent>
+          <DrawerHeader className="text-left">
+            <DrawerTitle>Edit profile</DrawerTitle>
+            <DrawerDescription>
+              Make changes to your profile here. Click save when you're done.
+            </DrawerDescription>
+          </DrawerHeader>
+          <div className="px-4">{children}</div>
+          <DrawerFooter className="pt-2">
+            <DrawerClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    );
+  } else {
+    return (
+      <Sheet open={true} onOpenChange={closeSheet}>
+        <SheetContent className="w-full min-w-[768px]">
+          <SheetHeader>
+            <SheetTitle>Edit profile</SheetTitle>
+            <SheetDescription>
+              Make changes to your profile here. Click save when you're done.
+            </SheetDescription>
+          </SheetHeader>
+          {children}
+          <SheetFooter className="pt-2">
+            <SheetClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </SheetClose>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
+    );
+  }
+});
 
 export const SheetStack = () => {
   const { sheetStack, closeSheet } = useSheetStack();
@@ -27,58 +77,10 @@ export const SheetStack = () => {
 
   if (sheetStack.length === 0) return null;
 
-  const StackItem = ({
-    key,
-    children
-  }: {
-    key: number;
-    children: ReactNode;
-  }) => {
-    if (isMobile) {
-      return (
-        <Drawer key={key} open={true} onOpenChange={closeSheet}>
-          <DrawerContent>
-            <DrawerHeader className="text-left">
-              <DrawerTitle>Edit profile</DrawerTitle>
-              <DrawerDescription>
-                Make changes to your profile here. Click save when you're done.
-              </DrawerDescription>
-            </DrawerHeader>
-            <div className="px-4">{children}</div>
-            <DrawerFooter className="pt-2">
-              <DrawerClose asChild>
-                <Button variant="outline">Cancel</Button>
-              </DrawerClose>
-            </DrawerFooter>
-          </DrawerContent>
-        </Drawer>
-      );
-    } else {
-      return (
-        <Sheet key={key} open={true} onOpenChange={closeSheet}>
-          <SheetContent className="w-full min-w-[768px]">
-            <SheetHeader>
-              <SheetTitle>Edit profile</SheetTitle>
-              <SheetDescription>
-                Make changes to your profile here. Click save when you're done.
-              </SheetDescription>
-            </SheetHeader>
-            {children}
-            <SheetFooter className="pt-2">
-              <SheetClose asChild>
-                <Button variant="outline">Cancel</Button>
-              </SheetClose>
-            </SheetFooter>
-          </SheetContent>
-        </Sheet>
-      );
-    }
-  };
-
   return (
     <>
       {sheetStack.map(({ component: Component, props }, index) => (
-        <StackItem key={index}>
+        <StackItem key={index} isMobile={isMobile} closeSheet={closeSheet}>
           <Component {...props} />
         </StackItem>
       ))}
